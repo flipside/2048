5fb061fc4c19a85532fb7a7c70eed03a5adf36fe
Mat Tyndall <mat.tyndall@gmail.com>
2014-03-11 22:16:42 -0700

    added directional buttons and tilt control

--- src/js/keyboard_input_manager.js
+++ src/js/keyboard_input_manager.js
@@ -65,13 +65,106 @@ KeyboardInputManager.prototype.listen = function () {
     drag_block_horizontal: true,
     drag_block_vertical: true
   });
-  
+
   handler.on("swipe", function (event) {
     event.gesture.preventDefault();
     mapped = gestures.indexOf(event.gesture.direction);
 
     if (mapped !== -1) self.emit("move", mapped);
   });
+
+  var directionButtons = document.getElementsByClassName("direction-button");
+
+  Hammer(directionButtons[0]).on("tap", function (e) {
+    e.preventDefault();
+    self.emit("move", 0);
+  });
+  Hammer(directionButtons[1]).on("tap", function (e) {
+    e.preventDefault();
+    self.emit("move", 1);
+  });
+  Hammer(directionButtons[2]).on("tap", function (e) {
+    e.preventDefault();
+    self.emit("move", 2);
+  });
+  Hammer(directionButtons[3]).on("tap", function (e) {
+    e.preventDefault();
+    self.emit("move", 3);
+  });
+
+  if (window.DeviceOrientationEvent) {
+    // var alpha = document.getElementById('alpha');
+    var beta = document.getElementById('beta');
+    var gamma = document.getElementById('gamma');
+    var tiltDirection = document.getElementById('tilt-direction');
+    var tilt = document.getElementById('tilt');
+    var neutralBeta, neutralGamma, b, g, db, dg;
+    var tiltMoved = false;
+    var tiltTimeout;
+
+    var tiltMove = function (direction) {
+      if (!tiltMoved) {
+        tiltMoved = true;
+        tiltTimeout = setTimeout(function () {
+          tiltMoved = false;
+        }, 600);
+        self.emit('move', direction);
+      }
+    };
+
+    var deviceorientation = function (e) {
+      // alpha.textContent = e.alpha;
+      b = e.beta;
+      g = e.gamma;
+      if (neutralBeta === undefined) {
+        neutralBeta = b;
+        neutralGamma = g;
+      }
+      db = neutralBeta - b;
+      dg = neutralGamma - g;
+
+      if (dg > 20) {
+        tiltDirection.textContent = 'left';
+        tiltMove(3);
+      } else if (dg < -20) {
+        tiltDirection.textContent = 'right';
+        tiltMove(1);
+      } else if (db > 15) {
+        tiltDirection.textContent = 'up';
+        tiltMove(0);
+      } else if (db < -15) {
+        tiltDirection.textContent = 'down';
+        tiltMove(2);
+      }
+
+      beta.textContent = db;
+      gamma.textContent = dg;
+      // tilt([e.beta, e.gamma]);
+    };
+
+    var tilted = Hammer(tilt, {
+      hold_timeout: 0
+    });
+    tilted.on('hold', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.gesture.preventDefault();
+      e.gesture.stopPropagation();
+      tilt.textContent = 'hold';
+      neutralBeta = undefined;
+      neutralGamma = undefined;
+      window.addEventListener('deviceorientation', deviceorientation, true);
+    });
+
+    tilted.on('release', function (e) {
+      e.preventDefault();
+      tilt.textContent = 'release';
+      window.removeEventListener('deviceorientation', deviceorientation, true);
+    });
+
+
+  }
+
 };
 
 KeyboardInputManager.prototype.restart = function (event) {
